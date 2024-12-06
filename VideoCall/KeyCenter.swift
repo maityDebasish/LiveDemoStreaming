e67ebc570960ae9a3d9cdf94d74045e59bca3871
Debjit Dutta <debjitdutta@Debjits-Laptop.local>
2024-12-06 14:38:32 +0530

    first commit

---
+++ VideoCall/KeyCenter.swift
@@ -0,0 +1,14 @@
+//
+//  KeyCenter.swift
+//  OpenLive
+//
+//  Created by GongYuhua on 6/25/16.
+//  Copyright © 2016 Agora. All rights reserved.
+//
+
+struct KeyCenter {
+    static let AppId: String = "3a5806d79cc24a838a2f0b9a7fddb96a"
+    
+    // assign token to nil if you have not enabled app certificate
+    static var Token: String? = nil
+}
